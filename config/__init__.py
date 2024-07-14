dc70b01f101ce089f5c4943a5c831bdb23882cc8
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2024-07-14 03:30:29 -0700

    Delete config directory

--- config/__init__.py
+++
@@ -1,6 +0,0 @@
-from .initialization import dirmake
-from .variables import (process_env_variable, str_to_bool, process_env_special, initialize_vars,
-                        create_vars, cleaner_value, vars_position, update_env_file)
-
-__all__ = ['process_env_variable', 'str_to_bool', 'process_env_special', 'initialize_vars', 'dirmake',
-           'create_vars', 'cleaner_value', 'vars_position', 'update_env_file']
