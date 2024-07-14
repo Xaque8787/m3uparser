ebc89856584494554c79ffcc8beea35596ba3b63
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2024-07-14 03:31:21 -0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from config import dirmake
-from config import process_env_variable, str_to_bool, process_env_special, initialize_vars, create_vars
-from processors import (prepare_m3us, clean_group_title, process_value, parse_m3u_file, proc_entries,
-                        process_live_tv_entries, sync_directories, move_files, clean_up)
-__all__ = ['prepare_m3us', 'clean_group_title', 'process_value', 'parse_m3u_file', 'proc_entries',
-           'process_live_tv_entries', 'sync_directories', 'move_files', 'clean_up', 'process_env_variable',
-           'str_to_bool', 'process_env_special', 'initialize_vars', 'dirmake', 'create_vars']
