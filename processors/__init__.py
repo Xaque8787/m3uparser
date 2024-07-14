6728e4b47c112b8ba65678413150ddce539bad20
Xaque8787 <65569846+Xaque8787@users.noreply.github.com>
2024-07-14 03:30:45 -0700

    Delete processors directory

--- processors/__init__.py
+++
@@ -1,9 +0,0 @@
-from .cleaners import process_value, clean_group_title, clean_up, enable_cleaners
-from .handlers import (proc_entries, process_live_tv_entries, prepare_m3us, sync_directories,
-                       move_files, handle_entry)
-from .parsers import parse_m3u_file, extract_key_value_pairs
-
-
-__all__ = ['clean_up', 'enable_cleaners', 'process_value', 'clean_group_title', 'proc_entries',
-           'process_live_tv_entries', 'extract_key_value_pairs', 'prepare_m3us', 'parse_m3u_file',
-           'sync_directories', 'move_files', 'handle_entry']
